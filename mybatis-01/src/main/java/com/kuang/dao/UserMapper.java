89b5626313c20fc639846baf40d1e43cc658ebdb
huangpei <1002072620@qq.com>
2021-03-22 23:09:52 +0800

    Mybatis第一次启动成功

---
+++ mybatis-01/src/main/java/com/kuang/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2021-03-22 19:39
+ */
+public interface UserMapper {
+    List<User> getUserList();
+}
---
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,31 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybaUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2021-03-22 20:00
+ */
+public class UserDaoTest {
+    @Test
+    public  void test(){
+        //第一部：从自己创建的工具类获得sqlSession对象
+        SqlSession sqlSession = MybaUtils.getSqlSession();
+        //执行SQL,面向接口编程，即实现UserDao接口的方法
+        UserMapper userDao = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+
+    }
+}
